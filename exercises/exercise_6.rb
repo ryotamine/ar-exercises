b14f9b984ae6f597346052ca8e4a550e50610385
Ryota Mine <ryotamine@gmail.com>
2019-04-16 21:50:44 +0000

    exercise 6 result

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -12,9 +12,7 @@ puts "----------"
 @store1.employees.create(first_name: "A", last_name: "B", hourly_rate: 60)
 @store2.employees.create(first_name: "C", last_name: "D", hourly_rate: 50)
 @store3.employees.create(first_name: "E", last_name: "F", hourly_rate: 40)
-
 @store4 = Store.find_by(id: 4)
 @store4.employees.create(first_name: "G", last_name: "H", hourly_rate: 30)
-
 @store5 = Store.find_by(id: 5)
 @store5.employees.create(first_name: "I", last_name: "J", hourly_rate: 20)
