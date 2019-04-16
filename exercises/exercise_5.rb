46e9b648719d89bd0b9f9aa41a6876d29f988890
Ryota Mine <ryotamine@gmail.com>
2019-04-16 21:31:23 +0000

    exercise 5 result

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total_revenue = Store.sum(:annual_revenue)
+puts total_revenue
+
+average_revenue = total_revenue / Store.count
+puts average_revenue
+
+@million = Store.where("annual_revenue >= '1000000'")
+puts @million.count
